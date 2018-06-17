301f26f96a41c68afa3d144646dca0e5ce7777ff
tatianamamonova <t_mamonovaa@outlook.com>
2018-06-17 15:24:41 +0300

    Add files via upload

--- rhyme_project.py
+++ rhyme_project.py
@@ -106,7 +106,7 @@ def stressed_syllables(cleaned_text):
 	stressed = []
 	body = cleaned_text
 	if len(body) == 0:
-		return [0]
+		return [1]
 	myurl = "https://ws3.morpher.ru/russian/addstressmarks"
 	req = urllib.request.Request(myurl)
 	req.add_header('Content-Type', 'text/plain; charset=utf-8')
@@ -135,7 +135,7 @@ def stressed_syllables(cleaned_text):
 		if out:
 			continue
 		else:
-			stressed.append(0)
+			stressed.append(int(count_syllables(word)/2) + 1)
 	return stressed
 
 def get_rhyme(lexeme):
@@ -279,5 +279,8 @@ def dictionary_app():
 		answer = transform(text)
 		return render_template("transformer.html", res=answer)
 	return render_template("transformer.html", res="")
-if __name__ == "__main__":
-    app.run(debug=True)
+	
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
